51085c79534d8886d8b9d0ba7108a14d735d70a0
parham_ktz <Pkootzari1383@gmail.com>
2025-05-04 13:16:14 +0330

    add some sample to phone-number vehicle

--- Phase_2/creating and inserting queries/03_vehicle.sql
+++ Phase_2/creating and inserting queries/03_vehicle.sql
@@ -7,7 +7,7 @@ CREATE TABLE vehicle (
   capacity SMALLINT NOT NULL,
   vehicle_type ENUM('Train', 'Flight', 'Bus') NOT NULL,
   PRIMARY KEY (vehicle_id),
-  CONSTRAINT check_capacity CHECK (capacity >= 10)
+  CONSTRAINT check_capacity CHECK (capacity > 0)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO vehicle (vehicle_id, capacity, vehicle_type) 
@@ -17,6 +17,20 @@ VALUES
   (3, 20, 'Bus'),
   (4, 18, 'Train'),
   (5, 12, 'Bus'),
-  (6, 12, 'Bus');
+  (6, 12, 'Bus'),
+  (7, 180, 'Flight'),
+  (8, 50, 'Bus'),
+  (9, 320, 'Train'),
+  (10, 45, 'Bus'),
+  (11, 200, 'Flight'),
+  (12, 60, 'Train'),
+  (13, 140, 'Flight'),
+  (14, 55, 'Bus'),
+  (15, 400, 'Train'),
+  (16, 38, 'Bus'),
+  (17, 220, 'Flight'),
+  (18, 100, 'Train'),
+  (19, 30, 'Bus'),
+  (20, 190, 'Flight');
 
 SELECT * FROM vehicle;
