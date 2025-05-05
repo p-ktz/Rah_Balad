9c934f3da6a6a5eaf43035c11aa62641f73d1d97
parham_ktz <Pkootzari1383@gmail.com>
2025-05-06 00:29:55 +0330

    correct the get_checked table TRIGGER

--- Phase_2/creating and inserting queries/15_get_checked.sql
+++ Phase_2/creating and inserting queries/15_get_checked.sql
@@ -23,20 +23,31 @@ BEFORE INSERT ON get_checked
 FOR EACH ROW
 BEGIN
   DECLARE role ENUM('Customer', 'Admin');
+  DECLARE user_exists INT;
 
   IF NEW.users_admin_id IS NOT NULL THEN
-    SELECT user_role INTO role
+    SELECT COUNT(*) INTO user_exists
     FROM users
     WHERE user_id = NEW.users_admin_id;
 
-    IF role != 'Admin' THEN
+    IF user_exists = 0 THEN
       SIGNAL SQLSTATE '45000'
-      SET MESSAGE_TEXT = 'Only Admins can check reservations.';
+      SET MESSAGE_TEXT = 'User does not exist.';
+    ELSE
+      SELECT user_role INTO role
+      FROM users
+      WHERE user_id = NEW.users_admin_id;
+
+      IF role != 'Admin' THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Only Admins can check reservations.';
+      END IF;
     END IF;
   END IF;
 END;
 //
 DELIMITER ;
+
 
 
 
@@ -47,20 +58,22 @@ VALUES
   (3, '2468135790', 'Reject', '2025-04-29 10:00:00'),
   (4, '2468135790', 'Accept', '2025-04-29 10:30:00'),
   (5, NULL, DEFAULT, '2025-04-29 11:00:00'),
-  (6, '1357924680', 'Accept', '2025-04-29 12:00:00'),
-  (7, '1357924680', 'Reject', '2025-04-29 12:15:00'),
+  (6, '3333444455', 'Accept', '2025-04-29 12:00:00'),
+  (7, '3333444455', 'Reject', '2025-04-29 12:15:00'),
   (8, '2468135790', 'Pending', '2025-04-29 12:30:00'),
-  (9, '9876543211', 'Accept', '2025-04-29 12:45:00'),
-  (10, '9876543211', 'Accept', '2025-04-29 13:00:00'),
-  (11, '1231231231', 'Reject', '2025-04-29 13:15:00'),
+  (9, '7878787878', 'Accept', '2025-04-29 12:45:00'),
+  (10, '7878787878', 'Accept', '2025-04-29 13:00:00'),
+  (11, '3333444455', 'Reject', '2025-04-29 13:15:00'),
   (12, NULL, DEFAULT, '2025-04-29 13:30:00'),
-  (13, '1357924680', 'Pending', '2025-04-29 13:45:00'),
-  (14, '1357924680', 'Accept', '2025-04-29 14:00:00'),
+  (13, '3333444455', 'Pending', '2025-04-29 13:45:00'),
+  (14, '3333444455', 'Accept', '2025-04-29 14:00:00'),
   (15, '2468135790', 'Reject', '2025-04-29 14:15:00'),
-  (16, '9876543211', DEFAULT, '2025-04-29 14:30:00'),
-  (17, '1231231231', 'Pending', '2025-04-29 14:45:00'),
+  (16, '7878787878', DEFAULT, '2025-04-29 14:30:00'),
+  (17, '3333444455', 'Pending', '2025-04-29 14:45:00'),
   (18, '2468135790', 'Accept', '2025-04-29 15:00:00'),
-  (19, '9876543211', 'Accept', '2025-04-29 15:15:00'),
-  (20, '1357924680', 'Reject', '2025-04-29 15:30:00');
+  (19, '7878787878', 'Accept', '2025-04-29 15:15:00'),
+  (20, '3333444455', 'Reject', '2025-04-29 15:30:00');
+
 
 SELECT * FROM rah_balad.get_checked;
+
